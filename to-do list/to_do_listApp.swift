619238aba6de555afe5c1e6d2872b8e9442fb2f6
Evan Zona <evanbzona@gmail.com>
2022-01-10 10:45:00 -0600

    Initial Commit

---
+++ to-do list/to_do_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  to_do_listApp.swift
+//  to-do list
+//
+//  Created by Student on 1/10/22.
+//
+
+import SwiftUI
+
+@main
+struct to_do_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
